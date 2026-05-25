6edaae9544f2dd412f6523808e34de0542e33d11
combastelclaire <claire.combastel@outlook.fr>
2026-05-25 22:46:09 +0200

    feat: responsive for CountryDetailPage + add country navigation #14

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import DashboardPage from './pages/DashboardPage'
+import CountryDetailPage from './pages/CountryDetailPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <DashboardPage />,
+  },
+  {
+    path: '/country/:id',
+    element: <CountryDetailPage />,
   },
 ])
 
